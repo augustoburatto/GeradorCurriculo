f59362c0fc1efc1d62d322dc478d40eb4891acba
augustoburatto <augusto_buratto@hotmail.com>
2021-08-25 21:20:41 -0300

    commit

--- js/jquery.js
+++ js/jquery.js
@@ -2,37 +2,38 @@ var countComp = 0;
 var countFormacao = 0;
 var countExper = 0;
 
-
-function addExperiencias()
-{
+function addExperiencias() {
     // var experiencias = document.getElementById('experiencias');
-    $('#experiencias').append('<div class="col-sm-12 form-group"><div class="col-sm-3 form-group"><label>Empresa</label><input id="empresa'+countExper+'" class="form-control" /></div><div class="col-sm-3 form-group"><label>Cargo</label><input id="cargo'+countExper+'" class="form-control" /></div><div class="col-sm-2 form-group"><label>De</label><input type="date" id="data_inicio'+countExper+'" class="form-control" /></div><div class="col-sm-2 form-group"><label>Até</label><input type="date" id="data_final'+countExper+'" class="form-control" /></div><button id="experiencia'+countExper+'_rmv" class="btn btn-danger" onClick="removeCat(this)">-</button></div>');
+    $('#experiencias').append('<div class="col-sm-12 form-group"><div class="col-sm-3 form-group"><label>Empresa</label><input id="empresa' + countExper + '" class="form-control" /></div><div class="col-sm-3 form-group"><label>Cargo</label><input id="cargo' + countExper + '" class="form-control" /></div><div class="col-sm-2 form-group"><label>De</label><input type="date" id="data_inicio' + countExper + '" class="form-control" /></div><div class="col-sm-2 form-group"><label>Até</label><input type="date" id="data_final' + countExper + '" class="form-control" /></div><button id="experiencia' + countExper + '_rmv" class="btn btn-danger" onClick="removeCat(this, \'experiencias\')">-</button></div>');
     countExper++;
 }
 
-function addFormacao()
-{
+function addFormacao() {
     // var formacao = document.getElementById('formacoes');
     // formacao.innerHTML +=
-    $('#formacoes').append('<div class="col-sm-12 form-group"><div class="col-sm-3 form-group"><label>Instituição</label><input id="instituicao'+countFormacao+'" class="form-control" /></div><div class="col-sm-3 form-group"><label>Curso</label><input id="curso'+countFormacao+'" class="form-control" /></div><div class="col-sm-3 form-group"><label>Conclusão</label><input type="date" id="conclusao'+countFormacao+'" class="form-control" /></div><button id="formacao'+countFormacao+'_rmv" class="btn btn-danger" onClick="removeCat(this)">-</button></div>');
+    $('#formacoes').append('<div class="col-sm-12 form-group"><div class="col-sm-3 form-group"><label>Instituição</label><input id="instituicao' + countFormacao + '" class="form-control" /></div><div class="col-sm-3 form-group"><label>Curso</label><input id="curso' + countFormacao + '" class="form-control" /></div><div class="col-sm-3 form-group"><label>Conclusão</label><input type="date" id="conclusao' + countFormacao + '" class="form-control" /></div><button id="formacao' + countFormacao + '_rmv" class="btn btn-danger" onClick="removeCat(this, \'formacao\')">-</button></div>');
     countFormacao++;
 }
 
-function addCompetencia()
-{
+function addCompetencia() {
     // var competencias = document.getElementById('competencias');
-    $('#competencias').append('<div class="col-sm-6 form-group"><label>Descrição</label><input id="comp'+countComp+'" class="form-control" /><button id="comp'+countComp+'_rmv" class="btn btn-danger" onClick="removeCat(this)">-</button></div>');
+    $('#competencias').append('<div class="col-sm-6 form-group"><label>Descrição</label><input id="comp' + countComp + '" class="form-control" /><button id="comp' + countComp + '_rmv" class="btn btn-danger" onClick="removeCat(this, \'competencias\')">-</button></div>');
     countComp++;
 }
 
-function removeCat(campo)
-{
+function removeCat(campo, form_id) {
     // document.getElementById(campo.id).parentElement.style.display = 'none';
-    $('#'+campo.id).parent().remove();
+    $('#' + campo.id).parent().remove();
+    if (form_id == "competencias") {
+        countComp--;
+    } else if (form_id == "formacao") {
+        countFormacao--;
+    } else {
+        countExper--;
+    }
 }
 
-function createPdf()
-{
+function createPdf() {
     var doc = new jsPDF('p', 'pt', 'letter');
     var specialElementHandlers = {
         '#editor': function (element, renderer) {
@@ -48,16 +49,16 @@ function createPdf()
     nasc = moment(nasc, "YYYY/MM/DD");
     nasc = nasc.format("DD/MM/YYYY");
 
-    var html = '<h1>'+nome+'</h1><p><strong>Endereço: </strong>'+address+'</p><p><strong>Telenfoe: </strong>'+tel+'</p><p><strong>E-mail: </strong>'+email+'</p><p><strong>Data de Nascimento: </strong>'+nasc+'</p><p><strong>_________________________________________________________________________________</strong></p><h3>Sobre</h3><p>'+sobre+'</p>';
+    var html = '<h1>' + nome + '</h1><p><strong>Endereço: </strong>' + address + '</p><p><strong>Telenfoe: </strong>' + tel + '</p><p><strong>E-mail: </strong>' + email + '</p><p><strong>Data de Nascimento: </strong>' + nasc + '</p><p><strong>_________________________________________________________________________________</strong></p><h3>Sobre</h3><p>' + sobre + '</p>';
 
     html += '<p><strong>_________________________________________________________________________________</strong></p>';
     html += '<h3>Histórico Profissional</h3>';
     for (e = 0; e < countExper; e++) {
-        var data_inicio = document.getElementById('data_inicio'+e).value;
+        var data_inicio = document.getElementById('data_inicio' + e).value;
         data_inicio = moment(data_inicio, "YYYY/MM/DD");
         data_inicio = data_inicio.format("DD/MM/YYYY");
-        var data_final = document.getElementById('data_final'+e).value;
-        if (document.getElementById('empresa'+e).value != '') {
+        var data_final = document.getElementById('data_final' + e).value;
+        if (document.getElementById('empresa' + e).value != '') {
             var periodo = '(' + data_inicio;
             if (data_final == '') {
                 periodo += ' - momento';
@@ -67,27 +68,27 @@ function createPdf()
                 periodo += ' - ' + data_final;
             }
             periodo += ")";
-            html += '<p><strong>Empresa: </strong>'+document.getElementById('empresa'+e).value+' '+periodo+'</p><p><strong>Cargo: </strong>'+document.getElementById('cargo'+e).value+'</p>';
+            html += '<p><strong>Empresa: </strong>' + document.getElementById('empresa' + e).value + ' ' + periodo + '</p><p><strong>Cargo: </strong>' + document.getElementById('cargo' + e).value + '</p>';
         }
     }
 
     html += '<p><strong>_________________________________________________________________________________</strong></p>';
     html += '<h3>Formação Acadêmica</h3>';
     for (f = 0; f < countFormacao; f++) {
-        var conclusao = document.getElementById('conclusao'+f).value;
+        var conclusao = document.getElementById('conclusao' + f).value;
         conclusao = moment(conclusao, "YYYY/MM/DD");
         conclusao = conclusao.format("DD/MM/YYYY");
-        if (document.getElementById('instituicao'+f).value != '') {
-            html += '<p><strong>Instituição e Curso: </strong>'+document.getElementById('instituicao'+f).value+' - '+document.getElementById('curso'+f).value+'</p><p><strong>Conclusão: </strong>'+conclusao+'</p>';
+        if (document.getElementById('instituicao' + f).value != '') {
+            html += '<p><strong>Instituição e Curso: </strong>' + document.getElementById('instituicao' + f).value + ' - ' + document.getElementById('curso' + f).value + '</p><p><strong>Conclusão: </strong>' + conclusao + '</p>';
         }
     }
 
     html += '<p><strong>_________________________________________________________________________________</strong></p>';
     html += '<h3>Competências</h3>';
     for (c = 0; c < countComp; c++) {
-        var comp = document.getElementById('comp'+c).value;
+        var comp = document.getElementById('comp' + c).value;
         if (comp != '') {
-            html += '<p>- '+comp+'</p>';
+            html += '<p>- ' + comp + '</p>';
         }
     }
 
